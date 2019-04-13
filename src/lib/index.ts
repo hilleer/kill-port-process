ccaed616243da6c662a820f501d76756642e80a2
hilleer <hiller@live.dk>
2019-04-13 11:44:58 +0200

    changes

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,24 +2,17 @@ import { platform } from 'os';
 
 import { Killer } from './killer';
 
-export default function(input, options: Options = {}) {
-	
+export default async function(input, options: Options = {}) {
+
 	try {
 		validateInput(input);
 		const mergedOptions = mergeOptions(options);
 		const ports = arrayifyInput(input).map(i => Number(i));
 
-		const killer = new Killer(ports, mergedOptions);
-
-		switch(platform()) {
-			case 'win32':
-				return killer.win32Kill();
-			default:
-				return killer.unixKill();
-		}
-		
+		const killer = new Killer(ports, mergedOptions, platform());
+		await killer.kill();
 	} catch (error) {
-		
+		throw new Error(error)
 	}
 }
 
@@ -38,7 +31,7 @@ export type Options = {};
 function mergeOptions(options: Options) {
 	const defaultOptions = {};
 
-	return { ...defaultOptions, options};
+	return { ...defaultOptions, options };
 }
 
 class InvalidInputError extends Error {
--- src/lib/killer.ts
+++ src/lib/killer.ts
@@ -1,5 +1,5 @@
 import * as pidFromPort from 'pid-from-port';
-import { exec } from 'child_process';
+import { exec, spawn } from 'child_process';
 import { promisify } from 'util';
 
 import { Options } from ".";
@@ -7,31 +7,30 @@ import { Options } from ".";
 const execAsync = promisify(exec);
 
 export class Killer {
-	private ports: number[];
-	private options: Options;
-	constructor(ports: number[], options: Options) {
+	protected ports: number[];
+	protected options: Options;
+	protected platform: NodeJS.Platform;
+	constructor(ports: number[], options: Options, platform) {
 		this.ports = ports;
 		this.options = options;
+		this.platform = platform
 	}
 
-	public async win32Kill() {
-		const promises = this.ports.map(pidFromPort);
-		const pids = await Promise.all(promises);
-	
-		const { stdout, stderr } = await execAsync(`TASKKILL /f /t /pid ${pids.join(' ')}`);
+	public async kill() {
+		const killFunc = this.platform === 'win32' ? this.win32Kill : this.unixKill;
+		const promises = this.ports.map(killFunc);
+		return Promise.all(promises);
+	}
+
+	private async win32Kill(port) {
+		const pid = await pidFromPort(port).catch(Promise.reject(`Failed to kill port ${port}`));
+		const { stdout, stderr } = await execAsync(`TASKKILL /f /t /pid ${pid}`);
 		stderr && console.log(stderr);
 		stdout && console.log(stdout);
 	}
 
-	public async unixKill() {
-		const promises = this.ports.map(this.unixKillHelper);
-		return Promise.all(promises);
-
-	}
-
-	private async unixKillHelper(input) {
-		const command = `lsof -i tcp:${input} | grep LISTEN | awk '{print $2}' | xargs kill -9`;
-		const { stdout, stderr } = await execAsync(command);
+	private async unixKill(port) {
+		const { stdout, stderr } = await execAsync(`lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`);
 		stderr && console.log(stderr);
 		stdout && console.log(stdout);
 	}
