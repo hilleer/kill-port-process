b03c141202be054857d5161bc05174ea65266baa
Daniel Hillmann <hiller@live.dk>
2019-07-16 18:44:58 +0200

    Clean up, add tests, optimise (#6)
    * Delete .eslintrc.json
    
    * add test skip test
    
    * add tests
    
    * changes

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,10 +1,10 @@
 import { createServer } from 'http';
 import { expect } from 'chai';
 
-import killPortProcess from '../src/lib';
+import killPortProcess, { arrayifyInput, validateInput, InvalidInputError } from '../src/lib';
 
 describe('index', () => {
-	describe('when called on a port', () => {
+	describe.skip('when called on a port', () => {
 		const PORT = 1234;
 
 		before((done) => {
@@ -19,6 +19,55 @@ describe('index', () => {
 				actualError = error;
 			}
 			expect(actualError).to.be.undefined;
+		});
+	});
+	describe('validateInput()', () => {
+		describe('when input is defined', () => {
+			it('should not throw', () => {
+				let error: any;
+				
+				try {
+					validateInput(1234);
+				} catch (err) {
+					error = error;
+				}
+				expect(error).to.be.undefined;
+			});
+		});
+		describe('when input is undefined', () => {
+			it('should throw an error', () => {
+				let error;
+				
+				try {
+					const input = undefined;
+					validateInput(input);
+				} catch (err) {
+					error = err;
+				}
+				expect(error).to.be.an.instanceof(InvalidInputError);
+			});
+		});
+	});
+	describe('arrayifyInput()', () => {
+		describe('when input is an array', () => {
+			it('should return as is', () => {
+				const input = [1234];
+
+				const actual = arrayifyInput(input);
+				const expected = [1234];
+
+				expect(actual).to.deep.eql(expected);
+			});
+		});
+		describe('when input is not of type array', () => {
+			it('should arrayify input', () => {
+				const input = 1234;
+
+				const actual = arrayifyInput(input);
+				const expected = [1234];
+
+				expect(actual).to.deep.eql(expected);
+			});
 		});
 	});
 });
