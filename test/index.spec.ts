8028d1e94558555e0ee8a68102c21cac89364bcd
Daniel Hillmann <hiller@live.dk>
2019-07-26 15:24:10 +0200

    make tests work!

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,25 +1,35 @@
 import { expect } from 'chai';
-import { createServer } from 'http';
 import { spawn } from 'child_process';
+import fetch, { FetchError } from 'node-fetch';
 
 import killPortProcess from '../src/lib/index';
 
 describe('index', () => {
-	describe.skip('when called on a port', () => {
-		const PORT = 1234;
-
-		before((done) => {
-			createServer((req, res) => res.end())
-				.listen(PORT, () => done());
+	describe('lala', () => {
+		let actualListen: string;
+		let expectedListen: string;
+		before('start a fake server', (done) => {
+			const child = spawn('node', ['test/fake-server.js']);
+			child.stdout.on('data', (data) => {
+				actualListen = data.toString();
+				expectedListen = 'Listening on 1234';
+				done();
+			});
+			child.stderr.on('data', (data) => {
+				console.log('stderr', data.toString());
+			});
 		});
-		it('should kill port', async () => {
-			let actualError;
-			try {
-				await killPortProcess(PORT);
-			} catch (error) {
-				actualError = error;
-			}
-			expect(actualError).to.be.undefined;
+		let actualFetchError: FetchError;
+		before('kill port, make request', async () => {
+			await killPortProcess(1234);
+			await fetch(`http://localhost:1234/`, { method: 'GET' })
+				.catch((reason) => actualFetchError = reason);
+		});
+		it('should actually listen on a server', () => {
+			expect(actualListen).to.be.equal(expectedListen);
+		});
+		it('should be true', () => {
+			expect(actualFetchError).to.be.an.instanceOf(FetchError);
 		});
 	});
 });
