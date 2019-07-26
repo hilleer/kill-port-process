67abaa1faf1d952bd71d43bb91fd39a73de8608b
Daniel Hillmann <hiller@live.dk>
2019-07-26 15:44:06 +0200

    fix index.d.ts for input to allow strings

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,11 @@
 
 declare function killPortProcess (input: any, options?: any): Promise<void>;
 
+type Input = number | number [] | string | string [];
+
 declare namespace killPortProcess {
 	export type Options = {};
- 	export function killPortProcess(input: number | number[], options?: Options): Promise<void>;
+ 	export function killPortProcess(input: Input, options?: Options): Promise<void>;
 }
 
 export = killPortProcess;
