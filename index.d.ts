667fa8ea1c28d7404818f492dba3146e7a167bb9
Daniel Hillmann <hiller@live.dk>
2019-08-13 11:32:37 +0200

    update d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,4 @@
+type Input = number | number[] | string | string[];
 
-declare function killPortProcess (input: any, options?: any): Promise<void>;
-
-type Input = number | number [] | string | string [];
-
-declare namespace killPortProcess {
-	export type Options = {};
- 	export function killPortProcess(input: Input, options?: Options): Promise<void>;
-}
-
-export = killPortProcess;
+export type Options = {};
+export function killPortProcess(input: Input, options?: Options): Promise<void>;
