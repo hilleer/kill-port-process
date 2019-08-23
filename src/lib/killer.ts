7563df832bd0517306dc16d87e525f9ad5da1f3e
Daniel Hillmann <hiller@live.dk>
2019-08-23 15:46:59 +0200

    add-node-6-8-12-to-travis-tests (#12)
    * add node 6 8 12 to travis tests
    
    * remove util.promisify to support node 6
    
    * remove platform to simplify ´

--- src/lib/killer.ts
+++ src/lib/killer.ts
@@ -1,37 +1,49 @@
 import { exec } from 'child_process';
+import { platform } from 'os';
 import * as pidFromPort from 'pid-from-port';
-import { promisify } from 'util';
 
 import { Options } from "./helpers";
-
-const execAsync = promisify(exec);
 
 export class Killer {
 	protected ports: number[];
 	protected options: Options;
-	protected platform: NodeJS.Platform;
-	constructor(ports: number[], options: Options, platform) {
+	constructor(ports: number[], options: Options) {
 		this.ports = ports;
 		this.options = options;
-		this.platform = platform;
 	}
 
 	public async kill() {
-		const killFunc = this.platform === 'win32' ? this.win32Kill : this.unixKill;
+		const killFunc = platform() === 'win32' ? this.win32Kill : this.unixKill;
 		const promises = this.ports.map(killFunc);
 		return Promise.all(promises);
 	}
 
 	private async win32Kill(port) {
 		const pid = await pidFromPort(port);
-		const { stdout, stderr } = await execAsync(`TASKKILL /f /t /pid ${pid}`);
-		stderr && console.log(stderr);
-		stdout && console.log(stdout);
+		return new Promise((resolve, reject) => {
+			exec(`TASKKILL /f /t /pid ${pid}`, (err, stdout, stderr) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				stderr && console.log(stderr);
+				stdout && console.log(stdout);
+				resolve();
+			});
+		});
 	}
 
 	private async unixKill(port) {
-		const { stdout, stderr } = await execAsync(`lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`);
-		stderr && console.log(stderr);
-		stdout && console.log(stdout);
+		return new Promise((resolve, reject) => {
+			exec(`lsof -i tcp:${port} | grep LISTEN | awk '{print $2}' | xargs kill -9`, (err, stdout, stderr) => {
+				if (err) {
+					reject(err);
+					return;
+				}
+				stderr && console.log(stderr);
+				stdout && console.log(stdout);
+				resolve();
+			});
+		});
 	}
 }
